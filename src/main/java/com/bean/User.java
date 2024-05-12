f2f4e2b630970102e57dfabbc2478375dcb098db
yelloredsleep <113323375+yelloredsleep@users.noreply.github.com>
2024-05-12 10:53:18 +0800

    Create User.java

---
+++ src/main/java/com/bean/User.java
@@ -0,0 +1,67 @@
+package com.bean;
+
+public class User {
+    private int user_id;
+    private String user_name;
+    private String user_pwd;
+    private String user_gender;
+    private int user_age;
+    private String user_introduce;
+    private String user_photo;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_pwd() {
+        return user_pwd;
+    }
+
+    public void setUser_pwd(String user_pwd) {
+        this.user_pwd = user_pwd;
+    }
+
+    public String getUser_gender() {
+        return user_gender;
+    }
+
+    public void setUser_gender(String user_gender) {
+        this.user_gender = user_gender;
+    }
+
+    public int getUser_age() {
+        return user_age;
+    }
+
+    public void setUser_age(int user_age) {
+        this.user_age = user_age;
+    }
+
+    public String getUser_introduce() {
+        return user_introduce;
+    }
+
+    public void setUser_introduce(String user_introduce) {
+        this.user_introduce = user_introduce;
+    }
+
+    public String getUser_photo() {
+        return user_photo;
+    }
+
+    public void setUser_photo(String user_photo) {
+        this.user_photo = user_photo;
+    }
+}
