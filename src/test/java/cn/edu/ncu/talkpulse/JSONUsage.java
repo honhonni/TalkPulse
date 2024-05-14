e129b82e656475548d31719214d99bbdc92653bc
honhonni <14411248+honhonni@user.noreply.gitee.com>
2024-05-14 11:54:58 +0800

    界面优化，test目录中新加json用法

--- src/test/java/cn/edu/ncu/talkpulse/JSONUsage.java
+++ src/test/java/cn/edu/ncu/talkpulse/JSONUsage.java
@@ -1,5 +1,9 @@
 package cn.edu.ncu.talkpulse;
 
+import cn.edu.ncu.talkpulse.group.entity.groupinfo;
+import cn.edu.ncu.talkpulse.group.service.GroupService;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +16,26 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @AutoConfigureMockMvc
-public class test {
+public class JSONUsage {
 
+    @Autowired
+    private GroupService groupService;
     @Test
     public void setUp() throws Exception {
 
         System.out.println("红色警告不用管，大概就是说你这个东西快过期了");
         System.out.println("想取消红色警告，在vm配置上加-Xshare:off  -XX:+EnableDynamicAgentLoading");
         System.out.println("vm是什么，自己搜");
-        System.out.println("hello");
+
+//        json数据转换格式
+//        用groupinfo做测试了
+//        像这种根据参数group_hostid准确查询具体一条数据的，不要用 list
+        JSONObject response = new JSONObject();
+        response.put("status", "201");
+        response.put("messages", "success");
+        List<groupinfo> list=groupService.selectAll(1);
+        response.put("data", list);
+
+        System.out.println(response);
     }
 }
