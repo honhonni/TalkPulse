e90e82ac242beb29e73fcc6cb89af98e1eb7f9ce
honhonni <14411248+honhonni@user.noreply.gitee.com>
2024-05-14 09:36:15 +0800

    界面优化，全局搜索框

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -2,4 +2,18 @@ $(function (){
     $('ul.navbar-left').on("click","li",function(){	// 用 on 不用管先后
         $(this).addClass('active').siblings().removeClass('active')
     })
+
+    $('#searchbutton').on('click',function (){
+        $('#searchModal').modal('show')
+    })
+    $('#searchModal').on('shown.bs.modal', function (event) {
+        $('#search').focus()
+        var listHeight = $('.search-list').outerHeight(false)
+        var modal_body = $('.modal-body')
+        if(listHeight > 300){
+            modal_body.css('height', 300 + 'px')
+        }else {
+            modal_body.css('height', listHeight + 'px')
+        }
+    })
 })
