fc2b991e5b8cb3d357d3e5a82cc2dd8909149672
yelloredsleep <113323375+yelloredsleep@users.noreply.github.com>
2024-05-31 18:49:34 +0800

    1

--- src/main/java/cn/edu/ncu/talkpulse/group/service/impl/GroupGroupValidationServiceImpl.java
+++ src/main/java/cn/edu/ncu/talkpulse/group/service/impl/GroupGroupValidationServiceImpl.java
@@ -40,16 +40,16 @@ public class GroupGroupValidationServiceImpl implements GroupValidationService {
 
     @Override
     public Boolean handlegroupvalidation(Byte agree, Integer group_validation_id) {
-        Groupvalidation groupvalidation=groupValidationDao.selectgroupvalidation(group_validation_id);
+        Groupvalidation groupvalidation=groupValidationDao.selectgroupvalidation(group_validation_id);//通过邀请号获取邀请信息
         if(groupvalidation==null){
             return false;
         }
         else {
-            groupValidationDao.readvalidation(group_validation_id);
-            if(agree==1){
-                Groupinfo groupinfo=applyDao.getgroupapplyByGroupId(groupvalidation.getGroupvalidation_groupid());
+            groupValidationDao.readvalidation(group_validation_id);//读了邀请信息
+            if(agree==1){//同意了邀请信息
+                Groupinfo groupinfo=applyDao.getgroupapplyByGroupId(groupvalidation.getGroupvalidation_groupid());//获取群聊相关信息，并将邀请信息传递到apply表格
                 applyDao.addgroupapply(groupvalidation.getGroupvalidation_receiverid(),LocalDateTime.now(),groupvalidation.getGroupvalidation_groupid(),groupinfo.getGroup_hostid(), String.valueOf(groupvalidation.getGroupvalidation_senderid()));
-                groupValidationDao.deletevalidation();
+                groupValidationDao.deletevalidation();//删除处理后的邀请信息
                 return true;
             }
             else {
